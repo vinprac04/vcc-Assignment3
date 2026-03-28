028416d3f65ebd76892896d63da2be2bc1619577
vinprac04 <54452485+vinprac04@users.noreply.github.com>
2026-03-29 00:55:31 +0530

    Update Grafana Code in the script

--- prom.sh
+++ prom.sh
@@ -144,6 +144,14 @@ scrape_configs:
       - targets: ["localhost:9100"]
 EOF'
 
+#  Install Grafana
+sudo mkdir -p /etc/apt/keyrings
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+sudo apt update && sudo apt install -y grafana
+
+
+
 # -----------------------------
 # 🔹 Start Services
 # -----------------------------
